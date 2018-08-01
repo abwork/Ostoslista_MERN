a1de9a034caa44733ca20a83230c6487d3c488dc
Abdi Muleta <abdi.muleta@integrify.io>
2018-08-01 18:28:43 +0300

    shoppinglist component commit
    ShoppingList component created with just local state with some dummy data

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button} from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import uuid from 'uuid';
+
+class ShoppingList extends Component {
+    state = {
+        items: [
+            { id: uuid(), name: 'Maito' },
+            { id: uuid(), name: 'Kukka' },
+            { id: uuid(), name: 'Banani' },
+            { id: uuid(), name: 'Kahvi' }
+        ]
+    }
+    render() {
+        const { items } = this.state;
+        return(
+            <Container>
+                <Button
+                  color="dark"
+                  style={{marginBottom: '2rem'}}
+                  onClick={() => {
+                    const name = prompt('Kirjoita Nimi');
+                    if(name) { 
+                        this.setState(state => ({
+                            items: [...state.items, { id: uuid(), name}]
+                        }));
+                    }
+                  }}
+                >
+                Lisätä
+                </Button>
+                <ListGroup>
+                    <TransitionGroup className="shopping-list">
+                        {items.map(({id, name}) => (
+                            <CSSTransition key={id} timeout={500} classNames="fade">
+                                <ListGroupItem>
+                                <Button 
+                                  className="remove-btn"
+                                  color="danger"
+                                  size="sm"
+                                  onClick={() => {
+                                    this.setState(state => ({
+                                        items: state.items.filter(item => item.id !== id)
+                                    }));
+                                }}
+                                >&times; 
+                                </Button>    
+                                    {name}
+                                </ListGroupItem>    
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
+            </Container>  
+        );
+    }
+ }
+
+ export default ShoppingList;
