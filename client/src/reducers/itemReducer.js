aa388f231b9dd288a2405e5a948180174cf0b25d
Abdi Muleta <abdi.muleta@integrify.io>
2018-08-03 11:58:56 +0300

    Front and backend connected commit
    Backend routes connected within the Redux actions using axios.  Add, delete and get implemented.

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,30 +1,32 @@
-import uuid from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const intialState = {
-    items: [
-        { id: uuid(), name: 'Maito' },
-        { id: uuid(), name: 'Kukka' },
-        { id: uuid(), name: 'Banani' },
-        { id: uuid(), name: 'Kahvi' }
-    ]
+    items: [],
+    loading: false
 }
 
 export default function(state = intialState, action){
     switch(action.type) {
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             };
         case ADD_ITEM:
             return {
                 ...state,
                 items: [action.payload, ...state.items]
-            };
+            }; 
         case DELETE_ITEM:
             return {
                 ...state,
-                items: state.items.filter(item => item.id !== action.payload)
+                items: state.items.filter(item => item._id !== action.payload)
+            };
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
             };
         default:
             return state;
