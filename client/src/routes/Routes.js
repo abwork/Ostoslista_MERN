b542510440e9d270541c95d4abcaa47128f9bb5a
Abdi Muleta <abdi.muleta@gmail.com>
2018-09-09 01:06:29 +0300

    Checkout implemented
    Checkout feature and more styling added

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -5,13 +5,15 @@ import { Switch, Route } from 'react-router-dom';
 // components
 import Home from '../containers/Home/Home';
 import Cart from '../containers/Cart/Cart';
+import Checkout from '../containers/Checkout/Checkout';
 
 
 const Routes = () => {
   return (
   <Switch>
     <Route path="/" component={ Home } exact />  
-    <Route path="/cart" component={ Cart } exact />      
+    <Route path="/cart" component={ Cart } exact />
+    <Route path="/checkout" component={ Checkout } exact />          
   </Switch>
   );
 };
