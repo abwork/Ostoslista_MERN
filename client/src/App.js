383c03711663d992dd4d25931f955cdbf7e5aa17
Abdi Muleta <abdi.muleta@gmail.com>
2018-09-08 22:10:22 +0300

    client setup
    create-reat-app, installed dependencied and environment clean for the frontend side dev

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>Navigation bar</div>
+        <div>Routes</div>
+      </div>
+    );
+  }
+}
+
+export default App;
